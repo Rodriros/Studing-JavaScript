1370183ad078380411cd2e660240bc11e454a6e4
Rodrigo Rosales Anicama <70780906+Rodriros@users.noreply.github.com>
2024-12-23 23:38:01 +0000

    Aula tipos Boolean , Null e Undefined

---
+++ Aulas node.js/tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Joaquim';
+const estaAprovado = true;
+
+if (estaAprovado === true) {
+    console.log('parabéns, boas festas');
+} else {
+    console.log('REPROVADO, até a próxima');
+}
+
+if (estudante === 'Joaquim') {
+    console.log(`olá, ${estudante}`);
+} else {
+    console.log('quem é você?')
+}
