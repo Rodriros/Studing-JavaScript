936749fde77dafb87906912c42d8655038cd7cb0
Rodrigo Rosales Anicama <70780906+Rodriros@users.noreply.github.com>
2024-12-23 22:48:59 +0000

    aula de strings

---
+++ Aulas node.js/strings.js
@@ -0,0 +1,35 @@
+const estudante = "João";
+const docente = "Clediosmario";
+const cumprimento = "nosso lema é 'estudar bastante!'";
+const citacao = `Professor diz: "nosso lema é 'estudar bastante!'"`
+
+console.log(cumprimento);
+console.log(citacao);
+
+
+//uso de template string
+console.log(`O estudante se chama ${estudante}`);
+
+
+//uso de função de caixa alta
+const senha = 'SenhaSegura123'+estudante.toUpperCase(0);
+
+console.log(senha);
+
+//testando caracteres especiais
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+let valor1 = 0
+let abacate
+let valor2 = "10"
+
